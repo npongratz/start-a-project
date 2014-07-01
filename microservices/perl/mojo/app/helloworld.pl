ac23df728c7958e62af90f77f609b4eb9510a00f
Brad Pinter <brad.pinter@gmail.com>
2014-06-30 19:59:35 -0500

    Perl mojo

--- microservices/perl/mojo/app/helloworld.pl
+++ microservices/perl/mojo/app/helloworld.pl
@@ -3,7 +3,7 @@ use Mojolicious::Lite;
 get '/hello/:foo' => sub {
   my $self = shift;
   my $foo  = $self->param('foo');
-  $self->render(text => "Hello from $foo?");
+  $self->render(text => "Hello from $foo.");
 };
 
 app->start;
